8ba39e359754536358a0786496cb0bcdfd7456eb
Mhdseomid <129630305+Mhdseomid@users.noreply.github.com>
2023-06-10 22:04:06 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-# test_add.py
 
 import unittest
 from add import add_numbers
 
-class TestAdd(unittest.TestCase):
-
+class TestAddNum(unittest.TestCase):
     def test_add_positive_numbers(self):
         self.assertEqual(add_numbers(2, 3), 5)
         self.assertEqual(add_numbers(5, 10), 15)
